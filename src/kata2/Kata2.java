79c3d6c99120471077b9ab6944e9f8724c39af39
thanq <thanq@DESKTOP-42KOIL4>
2020-01-14 14:35:28 +0000

    Version 5 Kata2- Cambiado los datos de Integer a String

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,17 +5,10 @@ import java.util.*;
 public class Kata2 {
 
     public static void main(String[] args) {
-        Integer [] data=new Integer[15];
-        for(int i=0;i<15;i++){
-            if(i%2==0){
-                data[i]=2;
-            }else{
-                data[i]=3;
-            }
-        }
+        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         Histogram histo=new Histogram(data);
-        Map<Integer,Integer> histogr = histo.getHistogram();
-        for(HashMap.Entry<Integer,Integer> i: histogr.entrySet()){
+        Map<String,Integer> histogr = histo.getHistogram();
+        for(HashMap.Entry<String,Integer> i: histogr.entrySet()){
             System.out.println(i.getKey()+"==>"+i.getValue());
         }
     }
