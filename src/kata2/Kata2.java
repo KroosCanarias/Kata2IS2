bff0d87d5fd412d160b00da2d2fdac45245113e0
thanq <thanq@DESKTOP-42KOIL4>
2020-01-14 13:24:10 +0000

    Version 1 KATA2

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,29 @@
+package kata2;
+
+import java.util.HashMap;
+
+public class Kata2 {
+
+    public static void main(String[] args) {
+        int [] data=new int[15];
+        for(int i=0;i<15;i++){
+            if(i%2==0){
+                data[i]=2;
+            }else{
+                data[i]=3;
+            }
+        }
+        HashMap <Integer,Integer> histogram=new HashMap<>();
+        for(Integer i: data){
+            if(histogram.containsKey(i)){
+                histogram.put(data[i],histogram.get(data[i])+1);
+            }else{
+                histogram.put(data[i],1);
+            }
+        }
+        for(HashMap.Entry<Integer,Integer> i: histogram.entrySet()){
+            System.out.println(i.getKey()+"==>"+i.getValue());
+        }
+    }
+    
+}
